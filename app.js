9af32374b0e7b4ee8c0adb2866866d5c1b2e257e
Hoai <dotrunghoai@gmail.com>
2021-02-06 21:15:46 +0700

    lan3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const config = require("config");
 const path = require("path");
-const cors = require("cors");
+// const cors = require("cors");
 
 const routerBrand = require("./routers/brandRouter");
 const routerCar = require("./routers/carRouter");
