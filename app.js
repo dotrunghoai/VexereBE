d99986e80b5341c647888df2dfccb725ba4fed63
Hoai <dotrunghoai@gmail.com>
2021-04-28 22:31:51 +0700

    lan 2

--- app.js
+++ app.js
@@ -22,12 +22,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
-    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+    "Origin, X-Requested-With, Content-Type, Accept, token"
   );
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH,HEAD,OPTIONS");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH");
   next();
 });
 app.use("/images", express.static(path.join(__dirname, "images")));
