6835181cb31f0dee9615d0f698617ed64bb384a2
Hoai <dotrunghoai@gmail.com>
2021-05-10 02:41:31 +0700

    lan 1

--- controllers/orderControl.js
+++ controllers/orderControl.js
@@ -1,4 +1,5 @@
 const Order = require("../models/orderModel")
+const Trip = require("../models/tripModel")
 
 const getOrder = async (req, res) => {
     try {
@@ -26,4 +27,37 @@ const getOrderByUser = async (req, res) => {
     }
 }
 
-module.exports = { getOrder, getOrderByUser }
+const deleteOrder = async (req, res) => {
+    try {
+        const { orderID } = req.query
+        const foundOrder = await Order.findById(orderID)
+        if (!foundOrder) {
+            return res.status(404).send({ message: 'The order not found!' })
+        }
+        const foundTrip = await Trip.findById(foundOrder.tripID)
+        if (!foundTrip) {
+            return res.status(404).send({ message: 'Thr trip not found!' })
+        }
+        if (foundTrip.startedDate < new Date()) {
+            return res.status(400).send({ message: 'The trip is over!' })
+        }
+        for (let index = 0; index < foundOrder.arrayOfSeat.length; index++) {
+            const seat = foundOrder.arrayOfSeat[index];
+            const foundSeat = foundTrip.arrayOfSeat.findIndex(
+                item => item.seatName === seat && item.status === 'booked'
+            )
+            if (foundSeat === -1) {
+                return res.status(400).send({ message: `Seat ${seat} has not been booked!` })
+            }
+            foundTrip.arrayOfSeat[foundSeat].userID = null
+            foundTrip.arrayOfSeat[foundSeat].status = 'available'
+        }
+        await foundTrip.save()
+        res.status(202).send({ message: 'The order has been deleted successfully' })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({ meesage: 'Something went wrong!' })
+    }
+}
+
+module.exports = { getOrder, getOrderByUser, deleteOrder }
--- routers/orderRouter.js
+++ routers/orderRouter.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const auth = require("../helpers/auth");
-const { getOrder, getOrderByUser } = require('../controllers/orderControl')
+const { getOrder, getOrderByUser, deleteOrder } = require('../controllers/orderControl')
 
 router.get('/order', getOrder)
 router.get('/orderByUser', auth(['user']), getOrderByUser)
+router.delete('/order', auth(['user']), deleteOrder)
 
 module.exports = router
